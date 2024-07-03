2309967eb6a1dbe036f0867d92bad149dee4029f
Adhe-Enne <ninadaniel_service@hotmail.com>
2024-07-03 20:50:33 -0300

    Agrego archivo 3

---
+++ 2 - Exercises/Poo.RepoTest/Poo.RepoTest/Test/Archivo3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo.RepoTest.Test
+{
+    internal class Archivo3
+    {
+    }
+}
